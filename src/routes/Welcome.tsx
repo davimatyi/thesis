50097d47e40301e4ae4baf002d1c2bfa29c6cc06
Mátyás Dávid <davimatyi@outlook.com>
2022-03-27 13:39:46 +0200

    Removed dropdown from welcome page

--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import Button from '../components/buttons/button/Button';
-import DropDownButton from '../components/buttons/dropdownbutton/DropDownButton';
 import LinkButton from '../components/buttons/linkbutton/LinkButton';
-import ColorPicker from '../components/colorpicker/ColorPicker';
 
 const Welcome: React.FC = () => {
   return (
     <>
       Welcome
       <LinkButton to="/editor">Next</LinkButton>
-      <DropDownButton text="dropdown">
-        {
-          ["asd", "sad", "dsa"].map((val, ind) => {
-            return <Button key={ind} onClick={() => console.log(val)}>{val}</Button>
-          })
-        }
-      </DropDownButton>
-      <ColorPicker initialColor='#555555' onColorPicked={(c: string) => alert(c)}></ColorPicker>
     </>
   );
 }
